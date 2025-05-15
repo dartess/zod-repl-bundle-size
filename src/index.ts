608b0f8cc7d3d3670d85125ebdb3184a8a0bd2a1
Sergey Kozlov <dartess@mail.ru>
2025-05-15 12:43:49 +0400

    initial

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod/v4-mini';
+
+const result = z.string().safeParse('');
+console.log(result);
+
+document.querySelector("#root")!.innerHTML = `
+<div class="content">
+  <h1>Vanilla Rspack</h1>
+  <p>Start building amazing things with Rspack.</p>
+</div>
+`;
